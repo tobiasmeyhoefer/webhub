63c4a03c032a1e0ba8bcf4a6001f2094753eb340
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-07-10 21:28:33 +0200

    added buy me a coffee

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import AuthButton from '@/components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function Dashboard() {
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,18 @@ import Link from 'next/link'
 export default async function Index() {
   return (
     <>
-      <AuthButton/>
+        <Link className='absolute top-8 left-8' href={'https://buymeacoffee.com/tobiasmeyhoefer'}>
+          Buy me a coffee
+        </Link>
+      <AuthButton />
       <div className="h-full flex justify-center items-center flex-col">
         <h1 className="font-bold md:text-6xl text-3xl leading-normal">
           Welcome at DevHub
         </h1>
         <p className="md:text-xl text-sm mb-10">made by @tobiasmeyhoefer</p>
-        <Link className='text-sm' href="/dashboard">Dashboard</Link>
+        <Link className="text-sm" href="/dashboard">
+          Dashboard
+        </Link>
       </div>
     </>
   )
