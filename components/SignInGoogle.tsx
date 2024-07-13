f97b216acc6eba4fdb9a6bfa2790e36f6a357202
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-07-13 14:02:14 +0200

    tried fixes

--- components/SignInGoogle.tsx
+++ components/SignInGoogle.tsx
@@ -4,18 +4,31 @@ import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
 
 const SignInGoogle = () => {
+
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+  }
+
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-white z-20">
       <form
         action={async () => {
           "use server"
           const supabase = createClient()
-          const redirectUrl = process.env.NODE_ENV === "development" ? `http://localhost:3000/auth/callback` : `https://webhub-cyan.vercel.app/auth/callback`
-          console.log(redirectUrl)
+          // const redirectUrl = process.env.NODE_ENV === "development" ? `http://localhost:3000/auth/callback` : `https://webhub-cyan.vercel.app/auth/callback`
+          // console.log(redirectUrl)
           const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-              redirectTo: redirectUrl
+              redirectTo: getURL()
             },
           })
 
